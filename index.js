836e82ef6c3bc419866f4dea8e4ced6688736d6b
Dominic Donahue <ddon335@UoA.auckland.ac.nz>
2021-07-03 17:53:17 +1200

    js test

---
+++ index.js
@@ -0,0 +1,14 @@
+file = document.getElementById("read")
+display = document.getElementById("singleCharacter")
+//previous = document.getElementById("left")
+//next = document.getElementById("right")
+
+const previous = function()
+{
+    alert("gay!!!!")
+}
+
+const next = function()
+{
+    alert("straight!!!!")
+}
