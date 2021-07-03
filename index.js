d730afc6afce7095eb883045747611dbca8582b9
Dominic Donahue <ddon335@UoA.auckland.ac.nz>
2021-07-03 18:43:40 +1200

    lmao

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-file = document.getElementById("read")
-display = document.getElementById("singleCharacter")
+window.onload = () =>
+{
+    const input = document.getElementById('input')
+    const display = document.getElementById("single")
+    let currentIndex = 0
+    let text = ""
+    input.onchange = () => {
+        textfile = input.files[0]
+        text = FileReaderSync.readAsText(textfile)
+        console.log(text)
+        updateDisplay()
+    }
+    const updateDisplay = () =>
+    {
+        display.innerHTML = text[currentIndex]
+    }
+    function next()
+    {
+        if (currentIndex == text.length)
+        {
+
+        } else 
+        {
+            currentIndex++
+            updateDisplay()
+        }
+    }
+
+    function previous()
+    {
+        if (currentIndex == 0)
+        {
+
+        }
+        else {
+            currentIndex--
+            updateDisplay()
+        }
+    }
+
+}
+
 //previous = document.getElementById("left")
 //next = document.getElementById("right")
 
-function next()
-{
-    alert("gay!!!!")
-}
 
-function previous()
-{
-    alert("straight!!!!")
-}
+
+
